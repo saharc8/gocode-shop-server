859dbb48d5b30530d9330ad2b6f35cce3b144f06
saharc8 <saharc8@gmail.com>
2021-06-06 22:04:04 +0300

    mongoose-crud

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 app.use(express.json());
+const mongoose = require("mongoose");
 
 app.get("/products", (req, res) => {
   const { category } = req.query;
@@ -9,105 +10,126 @@ app.get("/products", (req, res) => {
   let { max } = req.query;
   let categoryFilteredProductsArr;
   let minMaxFilteredProductsArr;
-  fs.readFile("products.json", "utf8", (err, products) => {
-    const productsArr = JSON.parse(products);
-    if (category) {
-      categoryFilteredProductsArr = productsArr.filter((product) =>
-        product.category.includes(category)
-      );
-    }
-    if (min && max) {
-      min = parseInt(min);
-      max = parseInt(max);
-      if (categoryFilteredProductsArr) {
-        minMaxFilteredProductsArr = categoryFilteredProductsArr.filter(
-          (product) => product.price > min && product.price < max
-        );
-      } else {
-        minMaxFilteredProductsArr = productsArr.filter(
-          (product) => product.price > min && product.price < max
+  Product.find()
+    .exec()
+    .then((productsArr) => {
+      if (category) {
+        categoryFilteredProductsArr = productsArr.filter((product) =>
+          product.category.includes(category)
         );
       }
-    }
-    if (minMaxFilteredProductsArr) {
-      res.send(minMaxFilteredProductsArr);
-    } else {
-      if (categoryFilteredProductsArr) {
-        res.send(categoryFilteredProductsArr);
-      } else {
-        res.send(productsArr);
+      if (min && max) {
+        min = parseInt(min);
+        max = parseInt(max);
+        if (categoryFilteredProductsArr) {
+          minMaxFilteredProductsArr = categoryFilteredProductsArr.filter(
+            (product) => product.price > min && product.price < max
+          );
+        } else {
+          minMaxFilteredProductsArr = productsArr.filter(
+            (product) => product.price > min && product.price < max
+          );
+        }
       }
-    }
-  });
+      if (minMaxFilteredProductsArr) {
+        res.send(minMaxFilteredProductsArr);
+      } else {
+        if (categoryFilteredProductsArr) {
+          res.send(categoryFilteredProductsArr);
+        } else {
+          res.send(productsArr);
+        }
+      }
+    });
 });
 
 app.get("/products/:id", (req, res) => {
-  fs.readFile("products.json", "utf8", (err, products) => {
-    const productsArr = JSON.parse(products);
-    const product = productsArr.find(
-      (product) => product.id === +req.params.id
-    );
-    if (product) {
-      res.send(product);
-    } else {
-      res.status(404);
-      res.send();
-    }
-  });
+  Product.findById(req.params.id)
+    .exec()
+    .then((product) => {
+      if (product) {
+        res.send(product);
+      } else {
+        res.status(404);
+        res.send();
+      }
+    });
 });
 
 app.post("/products", (req, res) => {
   console.log(req.body);
-  fs.readFile("products.json", "utf8", (err, products) => {
-    const productsArr = JSON.parse(products);
-    productsArr.push({
-      id: productsArr.length + 1,
-      title: req.body.title,
-      price: req.body.price,
-      description: req.body.description,
-      category: req.body.category,
-      image: req.body.image,
-    });
-    fs.writeFile("products.json", JSON.stringify(productsArr), (err) => {
-      console.log(err);
-      res.send("success");
-    });
+  const { title, price, description, category, image } = req.body;
+  Product.insertMany([
+    {
+      title,
+      price,
+      description,
+      category,
+      image,
+    },
+  ]).then(() => {
+    res.send("success");
   });
 });
 
 app.put("/products/:id", (req, res) => {
   console.log(req.body);
-  fs.readFile("products.json", "utf8", (err, products) => {
-    const productsArr = JSON.parse(products);
-    const { id } = req.params;
-    const { title, price, description, category, image } = req.body;
-    const updatedProductsArr = productsArr.map((product) => {
-      if (product.id === +id) {
-        return { ...product, title, price, description, category, image };
-      } else {
-        return product;
-      }
-    });
-    fs.writeFile("products.json", JSON.stringify(updatedProductsArr), (err) => {
-      console.log(err);
-      res.send("success");
-    });
+  const { id } = req.params;
+  const { title, price, description, category, image } = req.body;
+  Product.findOneAndUpdate(
+    { _id: id },
+    {
+      title,
+      price,
+      description,
+      category,
+      image,
+      s,
+    }
+  ).then(() => {
+    res.send("success");
   });
 });
 
 app.delete("/products/:id", (req, res) => {
   console.log(req.body);
-  fs.readFile("products.json", "utf8", (err, products) => {
-    const productsArr = JSON.parse(products);
-    const { id } = req.params;
-    const updatedProductsArr = productsArr.filter(
-      (product) => product.id !== +id
-    );
-    fs.writeFile("products.json", JSON.stringify(updatedProductsArr), (err) => {
-      console.log(err);
-      res.send("success");
-    });
+  const { id } = req.params;
+  Product.findOneAndDelete({ _id: id }).then(() => {
+    res.send("success");
   });
 });
 
-app.listen(6052);
+const productSchema = new mongoose.Schema({
+  title: String,
+  price: Number,
+  description: String,
+  category: String,
+  image: String,
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+// const product1 = {
+//   title: "Mens Cotton Jacket",
+//   price: 55.99,
+//   description: "great outerwear jackets for Spring/Autumn/Winter",
+//   category: "men's clothing",
+//   image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+// };
+
+// const product2 = {
+//   title: "Mens Cotton Jacket",
+//   price: 55.99,
+//   description: "great outerwear jackets for Spring/Autumn/Winter",
+//   category: "men's clothing",
+//   image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+// };
+
+mongoose
+  .connect("mongodb://localhost:27017/products", {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("connected");
+    app.listen(6052);
+  });
